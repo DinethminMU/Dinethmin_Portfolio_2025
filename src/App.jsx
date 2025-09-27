41775e4565401faa157a140f424debdd440139dc
DinethminMU <dinethminmu@gmail.com>
2025-09-27 08:40:54 +0530

    Background change

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
 
   return (
-    <section>
+    <section className="min-h-screen bg-black">
       <video src={video} autoPlay loop muted playsInline className="fixed inset-0 w-screen h-screen object-cover z-[-10] bg-black" />
       <NavBar />
       <div className="mt-28"></div>
