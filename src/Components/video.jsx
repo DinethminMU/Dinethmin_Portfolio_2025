6a3f47f7623c52ef390522534f8b7683bc554c8f
DinethminMU <dinethminmu@gmail.com>
2025-09-27 23:07:58 +0530

    Optimize videos

--- src/Components/video.jsx
+++
@@ -1,9 +0,0 @@
-{/* Video background layer */}
-      <video
-        src={video}
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="fixed inset-0 w-full h-full object-cover -z-10 bg-black"
-      />
