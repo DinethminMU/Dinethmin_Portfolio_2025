ddf211b78d5107494ed1f090e1b335928c91f31c
DinethminMU <dinethminmu@gmail.com>
2025-09-27 23:37:28 +0530

    update background

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -29,7 +29,7 @@ function NavBar() {
             DM
           </a>
           {/* Desktop Menu */}
-          <ul className="hidden md:flex space-x-6">
+          <ul className="hidden md:flex flex-wrap space-x-6">
         {navLinks.map((item) => (
             <li key={item.name}>
             <Link
