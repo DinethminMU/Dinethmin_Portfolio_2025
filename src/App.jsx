2a0032579d4b3d62d295e0d56477d45b7b10f6b8
DinethminMU <dinethminmu@gmail.com>
2025-09-28 00:36:59 +0530

    update background

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
   return (
     <>
      {/* Galaxy background */}
-      <div className="fixed inset-0 -z-10" >
-        <Galaxy mouseInteraction={true} disableAnimation={false} />
+      <div className="fixed inset-0 w-full h-full -z-10 " >
+        <Galaxy mouseInteraction={false} disableAnimation={false} />
       </div>
       
     
