9aec755a397f0c392f2cc07fd8177bfe3833ff29
DinethminMU <dinethminmu@gmail.com>
2025-09-28 00:44:22 +0530

    update background

--- src/Components/Galaxy.jsx
+++ src/Components/Galaxy.jsx
@@ -213,6 +213,9 @@ export default function Galaxy({
 
     let program;
 
+      let lastWidth = window.innerWidth;
+  let lastHeight = window.innerHeight;
+
    function resize() {
     const dpr = window.devicePixelRatio || 1;
     const width = window.innerWidth;
@@ -289,6 +292,13 @@ export default function Galaxy({
     }
     animateId = requestAnimationFrame(update);
     ctn.appendChild(gl.canvas);
+
+    gl.canvas.style.position = "absolute";
+    gl.canvas.style.top = "0";
+    gl.canvas.style.left = "0";
+    gl.canvas.style.width = "100%";
+    gl.canvas.style.height = "100%";
+    gl.canvas.style.pointerEvents = "none";
 
     function handleMouseMove(e) {
       const rect = ctn.getBoundingClientRect();
