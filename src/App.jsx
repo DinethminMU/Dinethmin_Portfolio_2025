500917e12e737d2995dcb817257dd88dbc8b3a1f
DinethminMU <dinethminmu@gmail.com>
2025-09-27 09:07:58 +0530

    Fix Video

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,33 @@ import Footer from "./Components/Footer";
 import About from "./Components/About";
 import Project2 from "./Components/Project2";
 
+
 function App() {
-
-
   return (
-    <section className="min-h-screen bg-black">
-      <video src={video} autoPlay loop muted playsInline className="fixed inset-0 w-full h-full object-cover z-[-10] bg-black" />
-      <NavBar />
-      <div className="mt-28"></div>
-      <Hero/>
-      <About/>
-      <Education/>
-      <Project2/>
-      <Skills/>
-      <Honors/>
-      <Contact/>
-      <Footer/>
-    </section>
+    <>
+      {/* Video background layer */}
+      <video
+        src={video}
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="fixed inset-0 w-full h-full object-cover -z-10 bg-black"
+      />
+      {/* Main content */}
+      <section className="relative min-h-screen z-0">
+        <NavBar />
+        <div className="mt-28"></div>
+        <Hero/>
+        <About/>
+        <Education/>
+        <Project2/>
+        <Skills/>
+        <Honors/>
+        <Contact/>
+        <Footer/>
+      </section>
+    </>
   );
 }
 
